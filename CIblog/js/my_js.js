c0bf2ab4ab8efa991c574608110fccdf6c3d093c
caimaoy <caimaoy@gmail.com>
2012-12-07 15:52:55 +0800

    CIBog
    以后就用这个管理了吧

---
+++ CIblog/js/my_js.js
@@ -0,0 +1,139 @@
+
+/*
+function startTime()
+{
+var today=new Date()
+var h=today.getHours()
+var m=today.getMinutes()
+var s=today.getSeconds()
+// add a zero in front of numbers<10
+m=checkTime(m)
+s=checkTime(s)
+document.getElementById('time').innerHTML=h+":"+m+":"+s
+t=setTimeout('startTime()',500)
+}
+
+function checkTime(i)
+{
+if (i<10) 
+  {i="0" + i}
+  return i
+}
+
+
+$(function)(){
+	$("#signin-email").bind({
+		keyup:function(){
+			if(this.value.length != 0){
+				$(this).next().hide();
+			}
+		},
+		blur:function(){
+			if(this.value.length == 0){
+				$(this).next().slideDown("fast");
+			}
+		}
+	});
+}
+
+
+$(function() {
+  // 你可以在这里继续使用$作为别名...
+  $("#signin-email").keyup(function(){
+  //$("lable[for = 'signin-email']").hide();
+  if(this.value.length != 0){
+  $(this).next().hide();
+  }
+});
+});
+
+$(function() {
+  // 你可以在这里继续使用$作为别名...
+  $("#signin-email").blur(function(){
+  //$("lable[for = 'signin-email']").hide();
+  if(this.value.length == 0){
+  $(this).next().slideDown("fast");
+  }
+});
+});*/
+
+$(function() {
+  // 你可以在这里继续使用$作为别名...
+  $("#signin-email").keyup(function(){
+  //$("lable[for = 'signin-email']").hide();
+  if(this.value.length != 0){
+  $(this).next().hide();
+  }
+}).blur(function(){
+  //$("lable[for = 'signin-email']").hide();
+  if(this.value.length == 0){
+  $(this).next().slideDown("fast");
+  }
+});
+});
+/*
+$(function() {
+  // 你可以在这里继续使用$作为别名...
+  $("#signin-email").blur(function(){
+  //$("lable[for = 'signin-email']").hide();
+  if(this.value.length == 0){
+  $(this).next().slideDown("fast");
+  }
+});
+});
+
+
+*/
+
+$(function() {
+  // 你可以在这里继续使用$作为别名...
+  $("#signin-password").keyup(function(){
+  //$("lable[for = 'signin-email']").hide();
+  if(this.value.length != 0){
+  $(this).next().hide();
+  }
+}).blur(function(){
+  //$("lable[for = 'signin-email']").hide();
+  if(this.value.length == 0){
+  $(this).next().slideDown("fast");
+  }
+});
+});
+
+
+/*
+jquery(document).ready(function() {
+	$("#signin-email").ready(function(){
+	if(this.value.length != 0){
+	$(this).next().hide();
+	}
+});
+});
+
+function test(){
+  var a = document.getElementById("ooo");
+  a.style.display = "none";
+  a.overflow = "hidden";
+}
+
+function test(){
+	var a = document.getElementById("signin-email");
+		var b = a.nextElementSibling;
+	if(a.value.length !=0){
+		b.style.display = "none" ;
+		//b.overflow = "hidden" ;
+	}
+}
+*/
+
+function load(){
+  var e = new Array();
+  e[0] = document.getElementById('signin-email');
+  e[1] = document.getElementById('signin-password');
+  
+  for (var i = e.length - 1; i >= 0; i--) {
+    if(e[i].value.length > 0){
+      e[i].nextElementSibling.style.display = "none";
+    }
+  };
+}
